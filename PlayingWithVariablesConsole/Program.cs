ad169be16a185573d13fd9ebde48d465490b5d34
LukeBuchmaier <labuchmaier@hotmail.com>
2022-09-18 20:46:53 -0400

    initial commit

---
+++ PlayingWithVariablesConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Variable Declaration
+int age;
+string name;
+string lastName;
+bool isAlive;
+char middleInitial;
+double heightInInches;
+decimal weightInLbs;
+
+//Variabel Initialization
+age = 25;
+name = "Luke";
+lastName = "Buchmaier";
+isAlive = true;
+middleInitial = 'A';
+heightInInches = 75;
+weightInLbs = 203.5m;
+
+
+//Variable Value
+Console.WriteLine($"{name} {middleInitial} {lastName} is {age} and {heightInInches} inches tall and weighs {weightInLbs}lbs. Is he alive: {isAlive} ");
+
